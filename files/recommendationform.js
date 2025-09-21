b678d76cc24fd12fc4687b7231428a6cc8e6cf91
Tenshi <158788527+basedTenshi@users.noreply.github.com>
2025-09-21 12:45:33 +0800

    Webpage Version 3.3 Update

---
+++ files/recommendationform.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("recommendationform");
+form.addEventListener("submit", function(submitLog) {})
+form.addEventListener("submit", function(submitLog);
+
+function submitLog(event){
+    submitMessage.textContent = "Form submitted!";
+    event.preventDefault();
+
+}
